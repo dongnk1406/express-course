4a47b6cd4ae0b1973c8f662d3e2b4a5188e9db1f
dongp6 <tryourbest146@gmail.com>
2023-05-28 16:45:30 +0700

    Feature: Get data from DB

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,5 +1,15 @@
-const getHomePage = (req, res) => {
-    return res.render("homepage.ejs")
+import db from "../models"
+
+const getHomePage = async (req, res) => {
+    try {
+        const data = await db.User.findAll()
+        return res.render("homepage.ejs", {
+            data: JSON.stringify(data)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
 }
 
 export default { getHomePage }
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser"
 import { configViewEngine } from "./config/viewEngine"
 import { initWebRoutes } from "./routes/web"
 import * as dotenv from 'dotenv'
+import { connectDB } from "./config/connectDB"
 dotenv.config()
 
 const app = express()
@@ -17,5 +18,8 @@ configViewEngine(app)
 
 // routes
 initWebRoutes(app)
+
+// connect DB
+connectDB()
 
 app.listen(port, () => console.log(`Backend Nodejs is running on http://localhost:${port}/`))
